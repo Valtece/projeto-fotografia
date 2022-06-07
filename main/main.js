a2c9b92b9734ad1898224625f694d6d3236a6134
Valtece Ventena <105180137+Valtece@users.noreply.github.com>
2022-06-07 20:22:53 -0300

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,7 +1,7 @@
 
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('#titulo', {distance: '-40px', duration: 4000})
+sr.reveal('#titulo', {distance: '-60px', duration: 4000})
 sr.reveal('.container-img', {distance: '-60px', duration: 4000})
 sr.reveal('#sobre', {distance: '-60px', duration: 4000})
-sr.reveal('#titulo-portfolio', {distance: '-50px', duration: 4000})
+sr.reveal('#titulo-portfolio', {distance: '-60px', duration: 4000})
