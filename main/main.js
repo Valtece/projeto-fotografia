0e22b7e1770a4aff4f8533049c0743b73991631f
Valtece Ventena <105180137+Valtece@users.noreply.github.com>
2022-06-07 20:04:23 -0300

    Update

---
+++ main/main.js
@@ -0,0 +1,7 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#titulo', {duration: 4000})
+sr.reveal('.container-img', {duration: 4000})
+sr.reveal('#sobre', {duration: 4000})
+sr.reveal('#titulo-portfolio', {duration: 4000})
