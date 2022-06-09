66402dcff346e89e20dd60045d31eb392af50854
Valtece Ventena <105180137+Valtece@users.noreply.github.com>
2022-06-08 22:37:04 -0300

    Update

--- main/main.js
+++ main/main.js
@@ -5,3 +5,5 @@ sr.reveal('#titulo', {distance: '-60px', duration: 4000})
 sr.reveal('.container-img', {distance: '-60px', duration: 4000})
 sr.reveal('#sobre', {distance: '-60px', duration: 4000})
 sr.reveal('#titulo-portfolio', {distance: '-60px', duration: 4000})
+sr.reveal('.section-portfolio', {distance: '-60px', duration: 4000})
+
